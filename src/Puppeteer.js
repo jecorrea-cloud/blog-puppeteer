beabae0622d2bd3341cff3794e1657ec408ee2f9
joaquin <jecorrea@protonmail.com>
2021-07-27 13:52:02 -0400

    puppeteer installed

---
+++ src/Puppeteer.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+(async () =>{
+    //try to retrieve info from
+    try{
+
+    }catch(err){
+        //if unsuccessful, throw an error
+        console.log(err);
+    }
+})()
+
