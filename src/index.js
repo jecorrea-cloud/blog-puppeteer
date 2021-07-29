67a8a1801e92115858c5b8cc715542d0cac16661
joaquin <jecorrea@protonmail.com>
2021-07-29 11:45:07 -0400

    Partially working

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// const puppeteer = require('puppeteer');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// // (async () =>{
+// //     //try to retrieve info from
+// //     try{
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+// //     }catch(err){
+// //         //if unsuccessful, throw an error
+// //         console.log(err);
+// //     }
+// // })()
+
+console.log("hii")
