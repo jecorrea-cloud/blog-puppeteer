ca8454c53d79905831aca4cf325a140a7d6e8924
joaquin <jecorrea@protonmail.com>
2021-07-30 07:35:50 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-// const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
 
-// // (async () =>{
-// //     //try to retrieve info from
-// //     try{
+(async () =>{
+    try{
+        
+        const browser = await puppeteer.launch();
+          
+        const webpage = await browser.newPage()
+        await webpage.goto('https://www.goal.com/en/premier-league/table/2kwbbcootiqqgmrzs6o5inle5')
+        // const html = await webpage.content()
+        // console.log(html)
 
+        let standings = await webpage.evaluate(()=>{
 
+            const teams = [...document.querySelectorAll('.widget-match-standings__team--full-name')].map((teamNode) => teamNode.innerText) 
+            const points = [...document.querySelectorAll('.widget-match-standings__pts')].map((pointsNode) => pointsNode.innerText) 
+            
+            return teams.map( (team, i) => ({team: team, points: points[i+1]}) )
+        })
 
-// //     }catch(err){
-// //         //if unsuccessful, throw an error
-// //         console.log(err);
-// //     }
-// // })()
+        console.log(standings)
+        browser.close()
 
-console.log("hii")
+    }catch(err){
+        console.log(err);
+    }
+})()
